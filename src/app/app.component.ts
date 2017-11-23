e4fcf4deacc5dae01c4d452210e75ffd9a7f43e3
grigoreme <grigore997@gmail.com>
2017-11-23 18:28:48 +0200

    Template init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public domains: string[] = [];
+
+  constructor() { }
+
+  updateDomain(domain) {
+    console.log(domain);
+  }
+  
 }
